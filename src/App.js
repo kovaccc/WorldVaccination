906b0d607c2238038edea3ed0c7b1f9ee333d286
Matej Kovačević <kovacevic@uhp.digital>
2021-05-31 22:50:33 +0200

    mapp added

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import "./App.css";
+import Map from './Map';
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <div className="content">
+        <Map></Map>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/WorldMap.js
@@ -0,0 +1,86 @@
+
+import React from "react";
+import * as d3 from "d3";
+const margin = 75;
+const width = 1200 - margin;
+const height = 650 - margin;
+class WorldMap extends React.Component {
+  componentDidMount() {
+    const { data } = this.props;
+    const svg = d3
+      .select(this.refs.chart)
+      .append("svg")
+      .attr("width", width)
+      .attr("height", height)
+      .append("g");
+    const projection = d3
+      .geoMercator()
+      .scale(130)
+      .translate([width / 2, height / 1.4]);
+    const path = d3.geoPath().projection(projection);
+    const map = svg
+      .selectAll("path")
+      .data(data.geoData.features)
+      .enter()
+      .append("path")
+      .attr("d", path)
+      .style("fill", "rgb(9, 157, 217)")
+      .style("stroke", "black")
+      .style("stroke-width", 0.5);
+    const nested = d3
+      .nest()
+      .key(d => d.year)
+      .rollup(leaves => {
+     
+         const total = d3.sum(leaves, d => d.attendance);
+         const coords = leaves.map(d => projection([+d.long, +d.lat]));
+         const center_x = d3.mean(coords, d => d[0]);
+         const center_y = d3.mean(coords, d => d[1]);
+         return {
+           attendance: total,
+           x: center_x,
+           y: center_y
+         };
+       })
+     .entries(data.cupData);
+    const attendance_extent = d3.extent(nested, d => d.value["attendance"]);
+    
+    const rScale = d3
+      .scaleSqrt()
+      .domain(attendance_extent)
+      .range([0, 8]);
+    svg
+      .append("g")
+      .attr("class", "bubble")
+      .selectAll("circle")
+      .data(nested.sort(function(a, b) {
+return b.value["attendance"] - a.value["attendance"];
+        })
+       )
+      .enter()
+      .append("circle")
+      .attr("fill", "rgb(247, 148, 42)")
+      .attr("cx", d => d.value["x"])
+      .attr("cy", d => d.value["y"])
+      .attr("r", d => rScale(d.value["attendance"]))
+      .attr("stroke", "black")
+      .attr("stroke-width", 0.7)
+      .attr("opacity", 0.7);
+    }
+    render() {
+      const { data } = this.props;
+      const styles = {
+        container: {
+          display: "grid",
+          justifyItems: "center"
+        }
+     };
+return (
+         <div ref="chart" style={styles.container}>
+           <p style={{ textAlign: "center" }}>Historical FIFA world cup geo map with React and D3.
+           </p>
+         </div>
+        );
+      }
+     }
+export default WorldMap;
