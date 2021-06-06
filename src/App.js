ea79651950aceba8deafab78d6b25ca4371d6631
Matej Kovačević <kovacevic@uhp.digital>
2021-06-06 18:32:35 +0200

    show daily informations feature finished

--- src/App.js
+++ src/App.js
@@ -1,40 +1,58 @@
 import React, { Component, useRef, useEffect, useState } from "react";
 import "./App.css";
-import Map from './components/Map';
+import WorldMap from './components/WorldMap';
 import VaccinationData from './components/VaccinationData'
+import DailyInformations from './components/DailyInformations'
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import * as Constants from './utils/Constants';
+import { count } from "d3-array";
 
 
 function App() {
 
-  const [currentCountry, setCurrentCountry] = useState();
   const [currentDate, setStartDate] = useState(new Date());
+  const [currentCountryData, setCountryData] = useState();
+  var selectedCountry;
   var vaccinationData = []
+  var vaccinationDataHeaders = []
 
-  const handleCountrySelectedCallback = (country) => {
-    console.log("country is", country)
-    setCurrentCountry(country)
+  const handleCountrySelectedCallback = (country, isSelected) => {
+    selectedCountry = country;
+  
+    if (selectedCountry === undefined || isSelected == false) {
+      setCountryData(new Array())
 
-    // todo check if country is undefined then dont draw barchart 
+    } else {
 
+      var countryData = vaccinationData.filter((rawData) => selectedCountry
+        .toLowerCase()
+        .replace(/ /g, "")
+        .includes(rawData.data[Constants.COUNTRY_NAME_COLUMN_INDEX].toLowerCase().replace(/ /g, ""))).map(it => it.data);
+
+        setCountryData(countryData)
+    }
   }
 
   const handleCountryDataCallback = (data) => {
     vaccinationData = data;
-    console.log("data is ", vaccinationData[0].data[0]); // access first element in first row 
+    vaccinationDataHeaders = vaccinationData[0].data;
+
+    // console.log("data is ", vaccinationData); // access first element in first row 
   }
 
 
   return (
-    <div className="App">
-      <div className="content">
-        <Map parentSelectedCountryCallback={handleCountrySelectedCallback}></Map>
+    <div className="App p-3">
+      <div className="appContent">
+        <WorldMap parentSelectedCountryCallback={handleCountrySelectedCallback}></WorldMap>
         <VaccinationData parentCountryDataCallback={handleCountryDataCallback}></VaccinationData>
-        <DatePicker selected={currentDate} onChange={(date) => setStartDate(date)} />
+        <div className="row justify-content-center">
+          <h1 className="col-4 mt-5"> Pick a date</h1>
+          <DatePicker selected={currentDate} onChange={(date) => setStartDate(date)} className="date-picker col-8 mt-5"/>
+        </div>
+        <DailyInformations countryData={currentCountryData} currentDate={currentDate}> </DailyInformations>
       </div>
-      <div id="chart"></div>
     </div>
   );
 }
---
+++ src/components/DailyInformations.js
@@ -0,0 +1,48 @@
+import dateFormat from "dateformat";
+import * as Constants from '../utils/Constants';
+import 'bootstrap/dist/css/bootstrap.min.css';
+const DailyInformations = ({ countryData, currentDate }) => {
+
+    var formatedDate = dateFormat(currentDate, "yyyy-mm-dd");
+
+    var filteredCountryData = new Array(15).fill(" ");
+
+    if (countryData === undefined || countryData.length == 0) {
+        filteredCountryData = new Array(15).fill(" ")
+    }
+    else {
+        filteredCountryData = countryData.find((data) => data[Constants.DATE_COLUMN_INDEX] == formatedDate) // find first that matches date 
+        if (filteredCountryData === undefined || filteredCountryData.length == 0) {
+            filteredCountryData = new Array(15).fill("")
+        }
+    }
+
+    function noDataAavailableString() {
+        return Constants.NO_DATA_AVAILABLE + formatedDate
+    }
+
+    return (
+        <div className="row countryDailyInformations">
+            <h2 className="col-12">Daily information</h2>
+            <div className="col-12 daily-informations-preview">
+                <p> <b> {Constants.COUNTRY_NAME_COLUMN}</b> {filteredCountryData[Constants.COUNTRY_NAME_COLUMN_INDEX] ? filteredCountryData[Constants.COUNTRY_NAME_COLUMN_INDEX] : noDataAavailableString()} </p>
+                <p> <b> {Constants.ISO_CODE_COLUMN}</b> {filteredCountryData[Constants.ISO_CODE_COLUMN_INDEX] ? filteredCountryData[Constants.ISO_CODE_COLUMN_INDEX] : noDataAavailableString()} </p>
+                <p> <b> {Constants.DATE_COLUMN}</b> {filteredCountryData[Constants.DATE_COLUMN_INDEX] ? filteredCountryData[Constants.DATE_COLUMN_INDEX] : noDataAavailableString()} </p>
+                <p> <b> {Constants.TOTAL_VACCINATIONS}</b> {filteredCountryData[Constants.TOTAL_VACCINATIONS_INDEX] ? filteredCountryData[Constants.TOTAL_VACCINATIONS_INDEX] : noDataAavailableString()} </p>
+                <p> <b> {Constants.PEOPLE_VACCINATED}</b> {filteredCountryData[Constants.PEOPLE_VACCINATED_INDEX] ? filteredCountryData[Constants.PEOPLE_VACCINATED_INDEX] : noDataAavailableString()} </p>
+                <p> <b> {Constants.PEOPLE_FULLY_VACCINATED}</b> {filteredCountryData[Constants.PEOPLE_FULLY_VACCINATED_INDEX] ? filteredCountryData[Constants.PEOPLE_FULLY_VACCINATED_INDEX] : noDataAavailableString()} </p>
+                <p> <b> {Constants.DAILY_VACCINATIONS_RAW}</b> {filteredCountryData[Constants.DAILY_VACCINATIONS_RAW_INDEX] ? filteredCountryData[Constants.DAILY_VACCINATIONS_RAW_INDEX] : noDataAavailableString()} </p>
+                <p> <b> {Constants.DAILY_VACCINATIONS}</b> {filteredCountryData[Constants.DAILY_VACCINATIONS_INDEX] ? filteredCountryData[Constants.DAILY_VACCINATIONS_INDEX] : noDataAavailableString()} </p>
+                <p> <b> {Constants.TOTAL_VACCIONATIONS_PER_HUNDRED}</b> {filteredCountryData[Constants.TOTAL_VACCIONATIONS_PER_HUNDRED_INDEX] ? filteredCountryData[Constants.TOTAL_VACCIONATIONS_PER_HUNDRED_INDEX] : noDataAavailableString()} </p>
+                <p> <b> {Constants.PEOPLE_VACCINATED_PER_HUNDRED}</b> {filteredCountryData[Constants.PEOPLE_VACCINATED_PER_HUNDRED_INDEX] ? filteredCountryData[Constants.PEOPLE_VACCINATED_PER_HUNDRED_INDEX] : noDataAavailableString()} </p>
+                <p> <b> {Constants.PEOPLE_FULLY_VACCINATED_PER_HUNDRED}</b> {filteredCountryData[Constants.PEOPLE_FULLY_VACCINATED_PER_HUNDRED_INDEX] ? filteredCountryData[Constants.PEOPLE_FULLY_VACCINATED_PER_HUNDRED_INDEX] : noDataAavailableString()} </p>
+                <p> <b> {Constants.DAILY_VACCINATIONS_PER_MILLION}</b> {filteredCountryData[Constants.DAILY_VACCINATIONS_PER_MILLION_INDEX] ? filteredCountryData[Constants.DAILY_VACCINATIONS_PER_MILLION_INDEX] : noDataAavailableString()} </p>
+                <p> <b> {Constants.VACCINES}</b> {filteredCountryData[Constants.VACCINES_INDEX] ? filteredCountryData[Constants.VACCINES_INDEX] : noDataAavailableString()} </p>
+                <p> <b> {Constants.SOURCE_NAME}</b> {filteredCountryData[Constants.SOURCE_NAME_INDEX] ? filteredCountryData[Constants.SOURCE_NAME_INDEX] : noDataAavailableString()} </p>
+                <p> <b> {Constants.SOURCE_WEBSITE}</b> {filteredCountryData[Constants.SOURCE_WEBSITE_INDEX] ? filteredCountryData[Constants.SOURCE_WEBSITE_INDEX] : noDataAavailableString()} </p>
+            </div>
+        </div>
+    );
+}
+
+export default DailyInformations;
